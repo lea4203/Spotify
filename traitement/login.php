d03a52fddef28cc338f8be111511d3e63ae540d9
KogeyR <remikogey@outlook.com>
2023-07-17 16:29:57 +0200

    delete playlist

--- traitement/login.php
+++ traitement/login.php
@@ -35,10 +35,10 @@ if ($user) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
 </head>
 <body>
-    <form action="login.php" method="POST">
+    <form action="" method="POST">
     <input type="text" name="username" id="username" placeholder="username">
     <input type="submit" value="Se connecter">
     </form>
